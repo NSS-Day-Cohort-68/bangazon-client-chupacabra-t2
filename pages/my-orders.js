973ae0408596805f28c58bff66c6451ada7aa235
Jordan VanVeldhuisen <jordanvanveldhuisen@gmail.com>
2024-03-29 15:20:48 -0400

    added jsx for correct information to display

--- pages/my-orders.js
+++ pages/my-orders.js
@@ -1,35 +1,42 @@
-import { useEffect, useState } from 'react'
-import CardLayout from '../components/card-layout'
-import Layout from '../components/layout'
-import Navbar from '../components/navbar'
-import Table from '../components/table'
-import { getOrders } from '../data/orders'
+import { useEffect, useState } from "react"
+import CardLayout from "../components/card-layout"
+import Layout from "../components/layout"
+import Navbar from "../components/navbar"
+import Table from "../components/table"
+import { getOrders } from "../data/orders"
 
 export default function Orders() {
   const [orders, setOrders] = useState([])
-  const headers = ['Order Date', 'Total', 'Payment Method']
+  const headers = ["Order Date", "Total", "Payment Method"]
 
   useEffect(() => {
-    getOrders().then(ordersData => {
+    getOrders().then((ordersData) => {
       if (ordersData) {
         setOrders(ordersData)
       }
     })
   }, [])
 
+  // Function to calculate total price of line items for an order
+  const calculateTotal = (lineItems) => {
+    let totalPrice = 0
+    lineItems.forEach((item) => {
+      totalPrice += item.product.price
+    })
+    return totalPrice.toFixed(2) // Adjust to 2 decimal places
+  }
+
   return (
     <>
       <CardLayout title="Your Orders">
         <Table headers={headers}>
-          {
-            orders.map((order) => (
-              <tr key={order.id}>
-                <td>{order.completed_on}</td>
-                <td>${order.total}</td>
-                <td>{order.payment_type?.obscured_num}</td>
-              </tr>
-            ))
-          }
+          {orders.map((order) => (
+            <tr key={order.id}>
+              <td>{order.created_date}</td>
+              <td>${calculateTotal(order.lineitems)}</td>
+              <td>{order.payment?.merchant_name}</td>
+            </tr>
+          ))}
         </Table>
         <></>
       </CardLayout>
